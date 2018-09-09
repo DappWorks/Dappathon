7a86b82eb591900409bb9c68f2dd8837e3a8695f
Amir Hagafny <amirh18@gmail.com>
2018-09-09 03:00:36 +0300

    Add v1.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dappathon
+The Dappathon website
+
+TODO:
+* call to action at the top
+* updated team member section
+* mobile menu (currently blank)
+* updated white on black logos for sponsors
+* going over text for spelling/grammar mistakes
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+    res.redirect("/dappathon");
+});
+
+
+app.get('/dappathon', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dappathon.html'));
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log("Running on Port " + port);
