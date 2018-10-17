32d07ccc2704dbe1bd684ae0562fb09b37bb786d
Amir Hagafny <amirh18@gmail.com>
2018-10-17 03:12:36 +0300

    Added Home Page

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use("/static", express.static(__dirname + "/static"));
 
-app.get('/', function (req, res) {
-    res.redirect("/dappathon");
+// app.get('/', function (req, res) {
+//     res.redirect("/dappathon");
+// });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-
-app.get('/dappathon', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dappathon.html'));
+app.get("/dappathon", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dappathon.html"));
 });
 
 var port = process.env.PORT || 3000;
